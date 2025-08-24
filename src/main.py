9124ee3c54746e193e93d8e86e10193c9e833edc
Anastasia Pilpani <hyperaquatic44@gmail.com>
2025-08-24 23:57:11 +0400

    added more call functions

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 import requests
 from dotenv import load_dotenv
 import requests
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Tuple
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
@@ -23,7 +23,11 @@ class TTCClient:
             request_params = {**self.default_params, **params}
         else:
             request_params = {**self.default_params}
-        response = self.session.get(url, params=request_params)
+        try:
+            response = self.session.get(url, params=request_params)
+        except requests.exceptions.HTTPError as e:
+            raise Exception(f"Request failed with status code {e.response.status_code}: {e.response.text}")
+
         response.raise_for_status()
         return response.json()
 
@@ -36,6 +40,43 @@ class TTCClient:
     def routes(self) -> List[Dict[str, Any]]:
         return self._get("/routes", {"modes": "BUS"})
 
+    def plan(self, from_coords: Tuple[float, float], to_coords: Tuple[float, float]) -> List[Dict[str, Any]]:
+        params = {
+            **self.default_params,
+            "fromPlace": f"{from_coords[0]},{from_coords[1]}",
+            "toPlace": f"{to_coords[0]},{to_coords[1]}",
+            "departMode": "leaveNow",
+            "modes": "WALK,BUS",
+            "optimize": "quick"
+        }
+        return self._get("/plan", params)
+
+    def bus_polyline(self, bus_id: str, forward: bool = True) -> List[Dict[str, Any]]:
+        params = {"forward": str(forward).lower()}
+        return self._get(f"/routes/1:{bus_id}/polyline", params)
+
+    def locations(self, bus_id: str, forward: bool = True) -> List[Dict[str, Any]]:
+        params = {"forward": str(forward).lower()}
+        return self._get(f"/routes/1:{bus_id}/positions", params)
+
+    def stop_routes(self, stop_id: str) -> List[Dict[str, Any]]:
+        params = self.default_params
+        return self._get(f"/stops/1:{stop_id}/routes", params)
+
+    def bus_routes(self, bus_id: str, forward: Optional[bool] = None) -> List[Dict[str, Any]]:
+        params = {
+            **self.default_params,
+            "forward": str(forward if forward is not None else False).lower()
+        }
+        return self._get(f"/routes/1:{bus_id}/stops", params)
+
+    def arrival_times(self, stop_id: str, ignore_scheduled_arrival_times: bool = False) -> List[Dict[str, Any]]:
+        params = {
+            **self.default_params,
+            "ignoreSchedulerdArrivalTimes": str(ignore_scheduled_arrival_times).lower()
+        }
+        return self._get(f"/stops/1:{stop_id}/arrival-times", params)
+
 
 if __name__ == "__main__":
     # Just exploring the data
@@ -44,4 +85,6 @@ if __name__ == "__main__":
     st = set()
     for stop in stops:
         st.add(stop.get('vehicleMode'))
+        if stop.get('vehicleMode') != "BUS":
+            print(stop)
     print(st)
